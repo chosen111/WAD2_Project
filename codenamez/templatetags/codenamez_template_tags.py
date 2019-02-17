542e456bbafd80ebe6252b5a02b9eff12d9530ea
Robert Neagu <robertneagu194@gmail.com>
2019-02-17 17:01:24 +0000

    added: button template and style

--- codenamez/templatetags/codenamez_template_tags.py
+++ codenamez/templatetags/codenamez_template_tags.py
@@ -3,5 +3,9 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('codenamez/tags/logo.html')
-def getLogoBySize(size="big"):
+def createLogoBySize(size="big"):
     return { 'size': size }
+
+@register.inclusion_tag('codenamez/tags/button.html')
+def createButton(id=None, text=""):
+    return { 'id': id, 'text': text }
