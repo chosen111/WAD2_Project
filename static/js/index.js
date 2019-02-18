7e57add36a2b3e58466e48b8275b430ceed8a526
Robert Neagu <robertneagu194@gmail.com>
2019-02-18 12:53:44 +0000

    added: overlay and fx on login click

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+    $(document).on('click', '.button#login', function() {
+        $('.overlay').remove();
+        
+        let $body = $('body');
+        let $overlay = $("<div>", { class: 'overlay' });
 
+        let $loginScreen = $("<section>", { class: 'login-section' }).appendTo($overlay);
+        $body.append($overlay);
+    })
 })
